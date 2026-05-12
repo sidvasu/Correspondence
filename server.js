8765e4c1e9cf77f0e162ee1714a3c9427fd39b88
sidvasu <siddharth.vasu.ca@gmail.com>
2026-05-12 14:30:40 -0700

    Added share page

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ import express from "express";
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { MongoClient, ObjectId } from "mongodb";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
+
+const uri = "mongodb://localhost:27017"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,4 +16,8 @@ app.use(express.static(join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(publicDir, "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
